1ffd35e868aa6046f81e10c91e5c4519d875dfd2
rahoofjamil <rahoof_1@live.de>
2022-12-16 14:02:09 +0100

    Initial Commit

---
+++ projekt_freitag/projekt_freitagApp.swift
@@ -0,0 +1,17 @@
+//
+//  projekt_freitagApp.swift
+//  projekt_freitag
+//
+//  Created by Rahoof Jamil on 16.12.22.
+//
+
+import SwiftUI
+
+@main
+struct projekt_freitagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
