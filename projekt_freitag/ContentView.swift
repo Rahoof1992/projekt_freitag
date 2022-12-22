2eee6b17ecd7099d2329e018123c7d71aaa82902
rahoofjamil <rahoof_1@live.de>
2022-12-22 13:55:05 +0100

    final commit

--- projekt_freitag/ContentView.swift
+++ projekt_freitag/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("schell").resizable().frame(width:150,height:150)
+                
+            Text("swift")
+        
         }
         .padding()
     }
